fe568e2a1d990846a4aaeae5a15d93acbfbf3844
Emil Nikolov <emil.e.nikolov@gmail.com>
2019-09-03 19:04:41 +0200

    fix tests

--- pkg/crypto/blake2b_test.go
+++ pkg/crypto/blake2b_test.go
@@ -10,12 +10,12 @@ import (
 
 var (
 	blake2bBytes = []byte{80, 159, 124, 221, 245, 42, 125, 177, 77, 112, 12, 60, 147, 143, 195, 92, 11, 59, 146, 209, 80, 78, 215, 8, 114, 147, 138, 9, 168, 93, 88, 196}
-	blake2bStr   = `112233445566778899 Saturn V rocket’s first stage carries 203,400 gallons (770,000 liters) of kerosene fuel and 318,000 gallons (1.2 million liters) of liquid oxygen needed for combustion. At liftoff, the stage’s five F-1 rocket engines ignite and produce 7.5 million pounds of thrust.`
+	blake2bStr   = []byte(`112233445566778899 Saturn V rocket’s first stage carries 203,400 gallons (770,000 liters) of kerosene fuel and 318,000 gallons (1.2 million liters) of liquid oxygen needed for combustion. At liftoff, the stage’s five F-1 rocket engines ignite and produce 7.5 million pounds of thrust.`)
 )
 
 func TestBlake2b(t *testing.T) {
 	cases := map[string]struct {
-		Input          string
+		Input          []byte
 		ExpectedOutput []byte
 	}{
 		"blake2b string": {
--- pkg/crypto/sha256_test.go
+++ pkg/crypto/sha256_test.go
@@ -10,12 +10,12 @@ import (
 
 var (
 	sha256Bytes = []byte{176, 189, 180, 212, 61, 57, 135, 83, 77, 129, 113, 121, 98, 174, 108, 84, 94, 242, 230, 210, 41, 15, 167, 31, 88, 176, 183, 225, 210, 120, 246, 34}
-	sha256Str   = `112233445566778899 Saturn V rocket’s first stage carries 203,400 gallons (770,000 liters) of kerosene fuel and 318,000 gallons (1.2 million liters) of liquid oxygen needed for combustion. At liftoff, the stage’s five F-1 rocket engines ignite and produce 7.5 million pounds of thrust.`
+	sha256Str   = []byte(`112233445566778899 Saturn V rocket’s first stage carries 203,400 gallons (770,000 liters) of kerosene fuel and 318,000 gallons (1.2 million liters) of liquid oxygen needed for combustion. At liftoff, the stage’s five F-1 rocket engines ignite and produce 7.5 million pounds of thrust.`)
 )
 
 func TestSha256(t *testing.T) {
 	cases := map[string]struct {
-		Input          string
+		Input          []byte
 		ExpectedOutput []byte
 	}{
 		"sha256 string": {
